e5364d9b1a37a61eb470e7a930b8a7b034b7e73c
Unofficialret3 <138513726+Unofficialret3@users.noreply.github.com>
2024-05-19 23:02:02 +0200

    "Updated index.php to include HTML head and body tags, added CSS link, and inserted PHP code for database connection and query, while also reformatting and reorganizing the sign in and sign up forms."

---
+++ App/Frontend/register.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Register</title>
+        <link rel="stylesheet" type="text/css" href="design.css">
+    </head>
+    <body>
+    <?php
+//basic information
+/*
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: ". $conn->connect_error);
+}
+
+// Prepare and bind
+$stmt = $conn->prepare("SELECT id, name, email FROM users WHERE name =?");
+$stmt->bind_param("s", $name);
+
+// Set parameters and execute
+$name = "John Doe";
+$stmt->execute();
+
+// Bind result variables
+$stmt->bind_result($id, $name, $email);
+
+// Fetch value
+if ($stmt->fetch()) {
+    echo $id. " ". $name. " ". $email;
+}
+
+// Close statement
+$stmt->close();
+
+// Close connection
+$conn->close();
+*/
+?>
+         <!-- Sign Up Form -->
+         <div id="central_panel">
+         <h1>Sign Up</h1>
+<form action="register.php" method="post">
+  <label for="username">Username:</label>
+  <input id="username" name="username" required="" type="text" />
+  <label for="email">Email:</label>
+  <input id="email" name="email" required="" type="email" />
+  <label for="password">Password:</label>
+  <input id="password" name="password" required="" type="password" />
+  <label for="street">Street and Number:</label>
+  <input id="street" name="street" type="text" />
+  <label for="city">City and Postal Code:</label>
+  <input id="city" name="city" type="text" />
+  <input name="register" type="submit" value="Register" />
+</form>
+         </div>
+
+        
+    </body>
+</html>
