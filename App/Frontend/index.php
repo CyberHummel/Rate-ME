b7a402bdd52d18f4331ba40e066007ec3e51e3e9
Unofficialret3 <138513726+Unofficialret3@users.noreply.github.com>
2024-05-20 15:43:36 +0200

    "Added database connection credentials and created a new mysqli connection"

--- App/Frontend/index.php
+++ App/Frontend/index.php
@@ -59,9 +59,12 @@ $conn->close();
 
 
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "rate_me";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
