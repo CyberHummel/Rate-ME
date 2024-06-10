329f06b62aeb3932ba3914b437f829d97fe9c5d3
Unofficialret3 <138513726+Unofficialret3@users.noreply.github.com>
2024-06-10 16:49:58 +0200

    "Updated redirect location in register.php from 'home.php' to '/App/Frontend/home.php'"

--- App/Backend/register.php
+++ App/Backend/register.php
@@ -82,7 +82,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Execute
     if ($stmt->execute()) {
        // echo "Registration successful!";
-        header("Location: home.php");
+        header("Location: /App/Frontend/home.php");
     } else {
         echo "Error registering user: " . $stmt->error;
     }
