2a30c2839822d5402513ad1c64384564a4dfc6bd
Flugel <TGKMarFlu@PAEDAGOGIK.SCHULEN.LOCAL>
2024-06-12 14:39:11 +0200

    "Added new database connection function for website and commented out old function"

--- App/connect.php
+++ App/connect.php
@@ -1,5 +1,7 @@
 <?php //<!--Aus Maximus's Gehirn-->
 //connect.php
+
+// lokal pokal
 function connect_db()
 {
     $sname = "localhost";
@@ -13,3 +15,18 @@ function connect_db()
         echo "Connection failed";
     }
 }
+/* website
+function connect_db()
+{
+    $sname = "sql212.infinityfree.com";
+    $unmae = "if0_36718971";
+    $password = "reiuogaVX7j7";
+    $db_name = "if0_36718971_rate_me";
+    $conn = mysqli_connect($sname, $unmae, $password, $db_name);
+    if ($conn) {
+        return $conn;
+    } else {
+        echo "Connection failed";
+    }
+}
+    */
