0fa85f48736392a71090e2c945cf78760631ba86
Unofficialret3 <138513726+Unofficialret3@users.noreply.github.com>
2024-06-12 17:59:03 +0200

    "Added database connection and user rating functionality to post.php, modified post display layout and rating system, and updated like/dislike button form."

--- App/post.php
+++ App/post.php
@@ -1,40 +1,70 @@
 <?php
 include_once 'get_username.php';
-// echo "User ID: " . $_SESSION['user_ID'];
+include_once 'connect.php';
 
-
-// eroor finden yaaay
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-function show_post($postid, $show_RatingButtons) //Maximus
+
+function show_post($postid, $show_RatingButtons)
 {
     if (isset($postid)) {
-        ?>
+?>
         <div class="container rounded shadow mb-3 bg-light text">
             <h5><?php echo get_Headline($postid); ?></h5>
             <div class="rounded shadow mb-8">
                 <img src="https://picsum.photos/50" alt="" class="profile-pic ml-2 mt-2">
                 <h6 class="ml-2">
                     <?php
-
                     $username = getPostCreator($postid);
                     echo $username;
-
                     ?>
-                </h6> <!--TODO: Get Post Creator/User-->
-                <i class="fa fa-star fa-2xl ml-2"></i>
-                <i class="fa fa-star fa-2xl"></i>
-                <i class="fa fa-star fa-2xl"></i>
-                <i class="fa fa-star fa-2xl mb-xl-4"></i>
+                </h6>
+                <?php
+                $username = getPostCreator($postid);
+                $sql = "SELECT user_rating FROM `user` WHERE user_username = '$username'";
+                $conn = connect_db();
+                $result = mysqli_query($conn, $sql);
+                $row = mysqli_fetch_assoc($result);
+                $userRating = $row['user_rating'];
+
+                if (is_null($userRating)) {
+                    echo 0.0;
+                } else {
+                    if ($userRating >= 20) { ?>
+                        <i class="fa fa-star fa-2xl" style="color: gold;"></i>
+                    <?php } else { ?>
+                        <i class="fa fa-star fa-2xl"></i>
+                    <?php }
+                    if ($userRating >= 40) { ?>
+                        <i class="fa fa-star fa-2xl" style="color: gold;"></i>
+                    <?php } else { ?>
+                        <i class="fa fa-star fa-2xl"></i>
+                    <?php }
+                    if ($userRating >= 60) { ?>
+                        <i class="fa fa-star fa-2xl" style="color: gold;"></i>
+                    <?php } else { ?>
+                        <i class="fa fa-star fa-2xl"></i>
+                    <?php }
+                    if ($userRating >= 80) { ?>
+                        <i class="fa fa-star fa-2xl" style="color: gold;"></i>
+                    <?php } else { ?>
+                        <i class="fa fa-star fa-2xl"></i>
+                    <?php }
+                    if ($userRating >= 95) { ?>
+                        <i class="fa fa-star fa-2xl" style="color: gold;"></i>
+                    <?php } else { ?>
+                        <i class="fa fa-star fa-2xl"></i>
+                    <?php }
+                }
+                ?>
             </div>
 
             <br>
             <div class="text-center rounded container shadow">
                 <br>
-                <img src="https://picsum.photos/200" alt=""> <!--TODO: Image in Database -->
+                <img src="https://picsum.photos/200" alt="">
                 <p style="text-align: left;" class="mt-2"><?php echo get_description($postid); ?></p>
-                <!--Multiple Spans for different Hashtags, also make clickable-->
                 <div class="text-left row mt-4">
                     <i class="fa fa-commenting-o fa-2xl ml-4"></i>
                     <?php
@@ -44,7 +74,6 @@ function show_post($postid, $show_RatingButtons) //Maximus
                     if (is_null($rating)) {
                         echo 0.0;
                     } else {
-                        // jetz einfach immer if else ob rating thresholds ereicht wurden oder nicht Marius, maximus
                         if ($rating >= 20) { ?>
                             <i class="fa fa-star fa-2xl" style="color: gold;"></i>
                         <?php } else { ?>
@@ -65,35 +94,33 @@ function show_post($postid, $show_RatingButtons) //Maximus
                         <?php } else { ?>
                             <i class="fa fa-star fa-2xl"></i>
                         <?php }
+                        if ($rating >= 95) { ?>
+                            <i class="fa fa-star fa-2xl" style="color: gold;"></i>
+                        <?php } else { ?>
+                            <i class="fa fa-star fa-2xl"></i>
+                        <?php }
                     }
 
-                    if($show_RatingButtons){
-
+                    if ($show_RatingButtons) {
                     ?>
 
-                    <!-- daumen hoch /runter form Marius-->
-                    <form action="like_post.php" method="post">
-                        <input type="hidden" name="post_id" value="<?php echo $postid; ?>">
-                        <input type="hidden" name="user_id" value="<?php echo $_SESSION['user_ID']; ?>">
-                        <button type="submit" name="like_type" value="like"><i class="fas fa-thumbs-up fa-2x"> </i>
-                        </button>
-                        <button type="submit" name="like_type" value="dislike"><i class="fas fa-thumbs-down fa-2x"></i>
-                        </button>
-                    </form>
-                        <?php } ?>
+                        <form action="like_post.php" method="post">
+                            <input type="hidden" name="post_id" value="<?php echo $postid; ?>">
+                            <input type="hidden" name="user_id" value="<?php echo $_SESSION['user_ID']; ?>">
+                            <button type="submit" name="like_type" value="like"><i class="fas fa-thumbs-up fa-2x"> </i>
+                            </button>
+                            <button type="submit" name="like_type" value="dislike"><i class="fas fa-thumbs-down fa-2x"></i>
+                            </button>
+                        </form>
+                    <?php } ?>
                     <h5>Views: <?php $ID = 11;
-                        if (is_null(get_views($ID))) echo 0; else echo get_views($ID); ?> </h5>
+                                if (is_null(get_views($ID))) echo 0;
+                                else echo get_views($ID); ?> </h5>
                 </div>
             </div>
         </div>
-
-        <?php
-
-
+<?php
     }
 }
 
 ?>
-
-
-
